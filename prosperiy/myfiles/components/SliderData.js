b7aeedfabaee0f06e12defeac147674dd47e5f32
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-11-04 13:50:01 +0530

    carousel set

---
+++ prosperiy/myfiles/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image:'/img/3.jpg'
+    },
+    {
+        image:'/img/4.jpg'
+    },
+    {
+        image:'/img/5.jpg'
+    },
+    {
+        image:'/img/6.jpg'
+    },
+    {
+        image:'/img/8.jpg'
+    },
+];
