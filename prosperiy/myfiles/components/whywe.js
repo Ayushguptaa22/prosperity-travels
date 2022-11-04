ab828a0cd237b3fcaa509e6c61a1540e9250a847
Ayush Gupta <ayujudo22@gmail.com>
2022-11-04 13:31:56 +0530

    styling

--- prosperiy/myfiles/components/whywe.js
+++ prosperiy/myfiles/components/whywe.js
@@ -9,6 +9,16 @@ function Whywe() {
         </h2>
       </div>
       <div className="container2">
+        <div className="cardLeftContent">
+          <div className="content">
+            <h3>This is content</h3>
+            <p>
+              DIn publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form of a document or
+              a typeface without relying on meaningful content.
+            </p>
+          </div>
+        </div>
         <div className="card">
           <div className="image">
             <img href="#" src="/img/5.jpg" />
--- prosperiy/myfiles/pages/index.js
+++ prosperiy/myfiles/pages/index.js
@@ -7,6 +7,9 @@ import MidContent from "../components/midContent";
 import AboutCards from "../components/aboutCards";
 import Whatwedo from "../components/whatwedo";
 import Careers from "../components/careers";
+import Footer from "../components/footer";
+import Slider from "../components/slider";
+
 export default function Home() {
   return (
     <div>
@@ -14,17 +17,16 @@ export default function Home() {
         <title>Prosperity Travels | Travel with Prosperity</title>
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
         <Link rel="preconnect" href="https://fonts.gstatic.com" />
-        <Link
-          href="https://fonts.googleapis.com/css2?family=Finlandica:wght@400;500&family=Poppins&display=swap"
-          rel="stylesheet"
-        />
       </Head>
+
       <Navbar />
       <MidContent />
       <Whywe />
       <Whatwedo />
       <AboutCards />
+      <Slider />
       <Careers />
+      <Footer />
     </div>
   );
 }
